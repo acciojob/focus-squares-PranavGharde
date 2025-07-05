bfb60368050905a2488a7a9690e3620a9d6860d0
PranavGharde <pranavgharde32@gmail.com>
2025-07-05 17:42:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ squares.forEach((square) => {
   square.addEventListener('mouseenter', () => {
     squares.forEach((sq) => {
       if (sq !== square) {
-        sq.style.backgroundColor = '#6F4E37'; 
+        sq.style.backgroundColor = '#6F4E37';
+      } else {
+        sq.style.backgroundColor = '#E6E6FA'; 
       }
     });
   });
