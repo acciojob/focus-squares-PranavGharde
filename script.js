e067e8b0d04ae806cb4879fde205085702d16114
PranavGharde <pranavgharde32@gmail.com>
2025-07-05 17:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ squares.forEach((square) => {
   square.addEventListener('mouseenter', () => {
     squares.forEach((sq) => {
       if (sq !== square) {
-        sq.style.backgroundColor = '#6F4E37';
+        sq.style.backgroundColor = '#6F4E37'; 
       }
     });
   });
+
   square.addEventListener('mouseleave', () => {
     squares.forEach((sq) => {
-      sq.style.backgroundColor = '#E6E6FA';
+      sq.style.backgroundColor = '#E6E6FA'; 
     });
   });
 });
